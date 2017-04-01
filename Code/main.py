6a2d838e679a73ce890fca370d34c11b1e278601
SachinDRaj <derycksachin@gmail.com>
2017-04-01 15:36:04 -0400

    fix errors with reading

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,5 @@
 import numpy
+import time
 
 def read_from_file(filepath):
     in_file = open(filepath,'r')
@@ -10,15 +11,17 @@ def get_from_string(contents):
     datalist = []
     all_lines = contents.split('\n')
     for line in all_lines:
-        arrival_time = line[0].strip(',')
-        service_start = line[1].strip(',')
-        service_end = line[2].strip(',')
-        drop_time = line[3].strip(',')
-        n_items = line[4].strip(',')
-        payment_type = line[5].strip(',')
-        wait_time = line[6].strip(',')
-        service_time = line[7].strip(',')
-        datalist.append(Customer(arrival_time,service_start,service_end,drop_time,n_items,payment_type,wait_time,service_time))
+        line = line.split(',')
+        cus_no = line[0].strip()
+        arrival_time = line[1].strip()
+        service_start = line[2].strip()
+        service_end = line[3].strip()
+        drop_time = line[4].strip()
+        n_items = line[5].strip()
+        payment_type = line[6].strip()
+        wait_time = line[7].strip()
+        service_time = line[8].strip()
+        datalist.append(Customer(cus_no,arrival_time,service_start,service_end,drop_time,n_items,payment_type,wait_time,service_time))
     return datalist
 
 class Queue:
@@ -38,17 +41,29 @@ class Queue:
         return len(self.items)
 
 class Customer:
-	def __init__(self, arrival_time, service_start_time, service_end_time, drop_time, number_items, payment_type, wait_time, service_time):
-		self.arrival_time = arrival_time
-		self.service_start_time = service_start_time
-		self.service_end_time = service_end_time
-		self.drop_time = drop_time
-		self.number_items = number_items
-		if payment_type == 0:
-			self.payment_type = 'cash'
-		elif payment_type == 1:
-			self.payment_type = 'credit card'
-		self.wait =wait_time
-		self.service_time = service_time
+    def __init__(self, customer_no, arrival_time, service_start_time, service_end_time, drop_time, number_items, payment_type, wait_time, service_time):
+        self.customer_no = customer_no
+        self.arrival_time = arrival_time
+        self.service_start_time = service_start_time
+        self.service_end_time = service_end_time
+        self.drop_time = drop_time
+        self.number_items = number_items
+        if payment_type == 0:
+            self.payment_type = 'cash'
+        elif payment_type == 1:
+            self.payment_type = 'credit card'
+        self.wait =wait_time
+        self.service_time = service_time
 
-read_from_file("Data/Express.csv")
+    def get_arrival_time(self):
+        return self.arrival_time
+    def get_customer_no(self):
+        return self.customer_no
+        
+
+datalist=read_from_file("Data/Express.csv")
+cust = datalist[1].get_arrival_time()
+print(cust)
+# struct_time = time.strptime("%H:%M:%S)
+
+# print(struct_time)
