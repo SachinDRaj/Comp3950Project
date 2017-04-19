f4888abf6e33f0d591b28abea9556f96a3828e34
essenjae <snjlewis42@gmail.com>
2017-04-18 20:42:19 -0400

    lane identifying

--- Code/simulation.py
+++ Code/simulation.py
@@ -84,9 +84,15 @@ def QSim(lambd,mu,simulation_time):
 def simulateGrocery(nServers, eServers,simulation_time,lambd,mu):
 
     for i in range(nServers):
+        print("")
+        print("nServers Summary Results for lane "+i+":")
+        print("")
         QSim(lambd,mu,simulation_time)
 
     for i in range(eServers):
+        print("")
+        print("eServers Summary Results for lane "+i+":")
+        print("")
         QSim(lambd,mu,simulation_time)
 
 
